bf3e86385a513c52546e1d16f88b26b9028557cf
Ordmund <vladyslav.alvgeir@gmail.com>
2025-10-08 23:52:49 +0900

    Added skeleton test prefab for testing character movements and animations.

--- Assets/Scripts/Constants/Addressables/AddressablesPaths.cs
+++ Assets/Scripts/Constants/Addressables/AddressablesPaths.cs
@@ -3,7 +3,7 @@ namespace Constants
     public static class AddressablesPaths
     {
         public const string WorldPrefab = "Assets/Addressables/World/World.prefab";
-        public const string PlayerPrefab = "Assets/Addressables/Player/Player.prefab";
+        public const string PlayerPrefab = "Assets/Prefabs/Addressables/Player/Player.prefab";
         public const string FirstPersonCameraPrefab = "Assets/Prefabs/Addressables/Camera/FirstPersonCamera.prefab";
     }
 }
